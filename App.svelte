2fa431f14950e188d0fae605312a4a150c8ce9b5
Cweili <3weili@gmail.com>
2019-05-08 09:42:27 +0800

    fix: fix styles

--- App.svelte
+++ App.svelte
@@ -85,69 +85,60 @@
 	}
 </script>
 
-<section class="todoapp">
-	<header class="header">
-		<h1>todos</h1>
-		<input
-			class="new-todo"
-			on:keydown={createNew}
-			placeholder="What needs to be done?"
-			autofocus
-		>
-	</header>
+<header class="header">
+	<h1>todos</h1>
+	<input
+		class="new-todo"
+		on:keydown={createNew}
+		placeholder="What needs to be done?"
+		autofocus
+	>
+</header>
 
-	{#if items.length > 0}
-		<section class="main">
-			<input id="toggle-all" class="toggle-all" type="checkbox" on:change={toggleAll} checked="{numCompleted === items.length}">
-			<label for="toggle-all">Mark all as complete</label>
+{#if items.length > 0}
+	<section class="main">
+		<input id="toggle-all" class="toggle-all" type="checkbox" on:change={toggleAll} checked="{numCompleted === items.length}">
+		<label for="toggle-all">Mark all as complete</label>
 
-			<ul class="todo-list">
-				{#each filtered as item, index (item.id)}
-					<li class="{item.completed ? 'completed' : ''} {editing === index ? 'editing' : ''}">
-						<div class="view">
-							<input class="toggle" type="checkbox" bind:checked={item.completed}>
-							<label on:dblclick="{() => editing = index}">{item.description}</label>
-							<button on:click="{() => remove(index)}" class="destroy"></button>
-						</div>
+		<ul class="todo-list">
+			{#each filtered as item, index (item.id)}
+				<li class="{item.completed ? 'completed' : ''} {editing === index ? 'editing' : ''}">
+					<div class="view">
+						<input class="toggle" type="checkbox" bind:checked={item.completed}>
+						<label on:dblclick="{() => editing = index}">{item.description}</label>
+						<button on:click="{() => remove(index)}" class="destroy"></button>
+					</div>
 
-						{#if editing === index}
-							<input
-								value='{item.description}'
-								id="edit"
-								class="edit"
-								on:keydown={handleEdit}
-								on:blur={submit}
-								autofocus
-							>
-						{/if}
-					</li>
-				{/each}
+					{#if editing === index}
+						<input
+							value='{item.description}'
+							id="edit"
+							class="edit"
+							on:keydown={handleEdit}
+							on:blur={submit}
+							autofocus
+						>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+
+		<footer class="footer">
+			<span class="todo-count">
+				<strong>{numActive}</strong> {numActive === 1 ? 'item' : 'items'} left
+			</span>
+
+			<ul class="filters">
+				<li><a class="{currentFilter === 'all' ? 'selected' : ''}" href="#/">All</a></li>
+				<li><a class="{currentFilter === 'active' ? 'selected' : ''}" href="#/active">Active</a></li>
+				<li><a class="{currentFilter === 'completed' ? 'selected' : ''}" href="#/completed">Completed</a></li>
 			</ul>
 
-			<footer class="footer">
-				<span class="todo-count">
-					<strong>{numActive}</strong> {numActive === 1 ? 'item' : 'items'} left
-				</span>
-
-				<ul class="filters">
-					<li><a class="{currentFilter === 'all' ? 'selected' : ''}" href="#/">All</a></li>
-					<li><a class="{currentFilter === 'active' ? 'selected' : ''}" href="#/active">Active</a></li>
-					<li><a class="{currentFilter === 'completed' ? 'selected' : ''}" href="#/completed">Completed</a></li>
-				</ul>
-
-				{#if numCompleted}
-					<button class="clear-completed" on:click={clearCompleted}>
-						Clear completed
-					</button>
-				{/if}
-			</footer>
-		</section>
-	{/if}
-</section>
-
-<footer class="info">
-	<p>Double-click to edit a todo</p>
-	<p>Written by <a href="https://twitter.com/Rich_Harris">Rich Harris</a></p>
-	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-</footer>
-
+			{#if numCompleted}
+				<button class="clear-completed" on:click={clearCompleted}>
+					Clear completed
+				</button>
+			{/if}
+		</footer>
+	</section>
+{/if}
